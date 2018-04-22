3200306460428a3d5d44bbc5bf32bb70a5abece9
Yve5 <yves.rouille@gmail.com>
2018-04-22 14:06:18 +0200

    Labs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Yve5Ng
+# Labs
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.1.
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('yve5-ng App', () => {
+describe('Labs App', () => {
   let page: AppPage;
 
   beforeEach(() => {
