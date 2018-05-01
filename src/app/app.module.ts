d6f3734e08c62b652791031b2eda48a18e1ad34e
Yve5 <yves.rouille@gmail.com>
2018-05-01 17:48:25 +0200

    copyright & more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './/routing.module';
+import { MoreComponent } from './more/more.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MoreComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { MoreComponent } from './more/more.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'more', component: MoreComponent }
 ];
 
 @NgModule({
